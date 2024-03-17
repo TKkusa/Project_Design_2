e0780ade35bbf1c611b5eb3460f76e94c5983224
SagaXSaga <116677818+SagaXSaga@users.noreply.github.com>
2024-03-18 02:59:18 +0800

    Add files via upload
    face landmark

---
+++ [mediapipe_python_tasks]_face_landmarker.py
@@ -0,0 +1,94 @@
+import cv2
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+from mediapipe.framework.formats import landmark_pb2
+import numpy as np
+import matplotlib.pyplot as plt
+import requests
+import os
+
+
+# Download the model file
+model_url = "https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task"
+model_filename = "face_landmarker_v2_with_blendshapes.task"
+
+if not os.path.exists(model_filename):
+    response = requests.get(model_url)
+    with open(model_filename, "wb") as f:
+        f.write(response.content)
+
+# Visualization utilities
+def draw_landmarks_on_image(rgb_image, detection_result):
+    face_landmarks_list = detection_result.face_landmarks
+    annotated_image = np.copy(rgb_image)
+
+    for idx in range(len(face_landmarks_list)):
+        face_landmarks = face_landmarks_list[idx]
+        face_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+        face_landmarks_proto.landmark.extend([
+            landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in face_landmarks
+        ])
+
+        mp.solutions.drawing_utils.draw_landmarks(
+            image=annotated_image,
+            landmark_list=face_landmarks_proto,
+            connections=mp.solutions.face_mesh.FACEMESH_TESSELATION,
+            landmark_drawing_spec=None,
+            connection_drawing_spec=mp.solutions.drawing_styles.get_default_face_mesh_tesselation_style())
+        mp.solutions.drawing_utils.draw_landmarks(
+            image=annotated_image,
+            landmark_list=face_landmarks_proto,
+            connections=mp.solutions.face_mesh.FACEMESH_CONTOURS,
+            landmark_drawing_spec=None,
+            connection_drawing_spec=mp.solutions.drawing_styles.get_default_face_mesh_contours_style())
+        mp.solutions.drawing_utils.draw_landmarks(
+            image=annotated_image,
+            landmark_list=face_landmarks_proto,
+            connections=mp.solutions.face_mesh.FACEMESH_IRISES,
+            landmark_drawing_spec=None,
+            connection_drawing_spec=mp.solutions.drawing_styles.get_default_face_mesh_iris_connections_style())
+
+    return annotated_image
+
+def plot_face_blendshapes_bar_graph(face_blendshapes):
+    face_blendshapes_names = [face_blendshapes_category.category_name for face_blendshapes_category in face_blendshapes]
+    face_blendshapes_scores = [face_blendshapes_category.score for face_blendshapes_category in face_blendshapes]
+    face_blendshapes_ranks = range(len(face_blendshapes_names))
+
+    fig, ax = plt.subplots(figsize=(12, 12))
+    bar = ax.barh(face_blendshapes_ranks, face_blendshapes_scores, label=[str(x) for x in face_blendshapes_ranks])
+    ax.set_yticks(face_blendshapes_ranks, face_blendshapes_names)
+    ax.invert_yaxis()
+
+    for score, patch in zip(face_blendshapes_scores, bar.patches):
+        plt.text(patch.get_x() + patch.get_width(), patch.get_y(), f"{score:.4f}", va="top")
+
+    ax.set_xlabel('Score')
+    ax.set_title("Face Blendshapes")
+    plt.tight_layout()
+    plt.show()
+
+
+# Create a FaceLandmarker object
+base_options = python.BaseOptions(model_asset_path='face_landmarker_v2_with_blendshapes.task')
+options = vision.FaceLandmarkerOptions(base_options=base_options,
+                                       output_face_blendshapes=True,
+                                       output_facial_transformation_matrixes=True,
+                                       num_faces=1)
+detector = vision.FaceLandmarker.create_from_options(options)
+
+# Detect face landmarks from the input image
+image = mp.Image.create_from_file("S__10772484.jpg")#change "S__10772484.jpg" to the file you need
+detection_result = detector.detect(image)
+
+# Process the detection result and visualize it
+annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
+cv2.imshow("Annotated Image", annotated_image)
+cv2.waitKey(0)
+
+# Visualize face blendshapes categories using a bar graph
+plot_face_blendshapes_bar_graph(detection_result.face_blendshapes[0])
+
+# Print the transformation matrix
+print(detection_result.facial_transformation_matrixes)
